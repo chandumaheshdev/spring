31840f25db5d8b19563a215ee7f2b766203afdf7
chandumaheshrsnit <chandumahesh90@gmail.com>
2017-06-05 15:28:47 +0530

    code demo rough prototype part  1 finished

--- spring demo one/src/com/luv2code/springdemo/BaseballCoach.java
+++ spring demo one/src/com/luv2code/springdemo/BaseballCoach.java
@@ -1,6 +1,6 @@
 package com.luv2code.springdemo;
 
-public class BaseballCoach {
+public class BaseballCoach implements Coach{
 
 	public String getDailyWorkout(){
 		return "spend 30 min on batting";
