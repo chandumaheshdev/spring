6d872c69c7ac6b828165dd2341de722b8c6ffef5
chandumaheshrsnit <chandumahesh90@gmail.com>
2017-06-09 14:28:38 +0530

    47.finished

--- spring demo one/src/com/luv2code/springdemo/BeanScopeDemoApp.java
+++ spring demo one/src/com/luv2code/springdemo/BeanScopeDemoApp.java
@@ -6,12 +6,22 @@ public class BeanScopeDemoApp {
 
 	public static void main(String[] args) {
 		//load the spring configuration file
-		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("beanScope-applicationContext2.xml");
 		//retrieve bean from spring container
 		
 		Coach theCoach=context.getBean("myCoach",Coach.class);
-		
+		 
 		Coach alphaCoach=context.getBean("myCoach",Coach.class);
+		
+		boolean result=(theCoach == alphaCoach);
+		
+		System.out.println("\n pointing to the same object: "+result);
+		
+		System.out.println("\n Memory location for the coach:"+theCoach);
+		
+		System.out.println("\n Memory location for alpha coach" +alphaCoach+ "\n");
+		
+		context.close();
 
 	}
 
